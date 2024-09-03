b8dd86e3c50fbb595c51ab13d76153a9cc926889
Abdousoilih95 <Abdou.soilihi95@gmail.com>
2024-09-04 00:12:08 +0200

    projects page ok without data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
-import Footer from '../footer/footer.tsx';
+import Footer from '../footer/footer';
+import ProjectPage from '../ProjectPage/ProjectPage';
+
+
+
 
 function App() {
+
+
   return (
     <div className="app">
+    <ProjectPage/>
     <Footer/>
     </div>
   );
