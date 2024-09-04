768f7df04b12769218a6b7f57c7e0648aeb88f1e
Nhi-Mehl <nhi.mehl@gmail.com>
2024-09-04 10:46:46 +0200

    refactor: :building_construction: Add folder and component Header, included Navbar and Banner dans Header, linked Header in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from '../Header/Header';
 import HomePage from '../HomePage/HomePage';
 import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <HomePage />
       <Footer />
     </div>
