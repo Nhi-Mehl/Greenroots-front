1f12412a4e538de1ee9904e02c773a54226cf746
Nhi-Mehl <nhi.mehl@gmail.com>
2024-09-04 16:57:59 +0200

    feat: :sparkles: Add style DetailTreePage

--- src/components/DetailTreePage/DetaiTreePage.tsx
+++ src/components/DetailTreePage/DetaiTreePage.tsx
@@ -1,14 +1,18 @@
 function DetailTreePage() {
   return (
-    <main>
-      <section>
-        <h2>Tire du projet</h2>
+    <main className="p-20">
+      <section className="w-full relative inline-block">
         <img
-          src="../../../../../public/images/project-3-home.jpg"
+          className="w-4/5 h-200 object-cover mx-auto"
+          src="/images/id9-HêtreCommun.jpg"
           alt="nom arbre"
         />
+
+        <h2 className="absolute inset-0 flex items-center justify-center h1-title text-white">
+          Tire du projet
+        </h2>
       </section>
-      <section>
+      <section className="w-4/5 my-20 mx-auto sectionText">
         <p>Nom de l’arbre: Groot</p>
         <p>Nom scientifique : Grout Arborius Titanicus</p>
         <p>
@@ -20,12 +24,14 @@ function DetailTreePage() {
         <p>Co2 : Absobre 100 tonnes de co2 par an</p>
         <p>Prix : 8€</p>
       </section>
-      <section>
-        <div>
+      <section className="flex justify-center gap-x-40 sectionText">
+        <div className="">
           <p>Quantité disponible</p>
           <p>12</p>
         </div>
-        <button type="button">Ajouter au panier</button>
+        <button className="btn" type="button">
+          Ajouter au panier
+        </button>
         <div>
           <p>Quantité planté</p>
           <p>500</p>
