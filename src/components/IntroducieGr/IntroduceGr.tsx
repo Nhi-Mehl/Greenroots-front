068a84f8614fe64dcbadf009534a831c270a7d9d
Abdousoilih95 <Abdou.soilihi95@gmail.com>
2024-09-05 00:16:55 +0200

    introducing page finished

---
+++ src/components/IntroducieGr/IntroduceGr.tsx
@@ -0,0 +1,52 @@
+
+
+function IntroducGr() {
+
+
+    return(
+        <div className="mr-28 ml-28 mb-12 text-justify">
+            <h1 className="p-6 text-3xl text-center ">GreenRoots en quelques mots</h1>
+            <p>Chez Greenroots, nous croyons que chaque arbre planté est une graine d'espoir pour l'avenir. Notre aventure a commencé avec une simple idée : lutter contre le changement 
+                climatique et restaurer les écosystèmes à travers le monde, un arbre à la fois.</p>
+
+
+                <h2 className="p-2  h3-title text-greenRegular">Comment tout a commencé ?</h2>
+            <p>L'histoire de Greenroots débute en 2024 avec un petit groupe de passionnés d'environnement convaincus que chacun peut jouer un rôle dans la protection de notre planète. Face à la déforestation massive et à ses 
+                impacts dévastateurs sur la biodiversité et le climat, nous avons décidé d'agir.</p>
+            <p>Inspirés par des initiatives locales de reforestation, nous avons uni nos forces pour créer une organisation ayant un impact global, mais avec des actions concrètes sur le terrain. Notre premier projet a vu le jour dans [pays d’origine ou première région], où nous avons collaboré 
+                avec des communautés locales pour restaurer des zones forestières endommagées.</p>
+
+
+                <h2 className="p-2  h3-title text-greenRegular">Notre Mission : Une Reforestation Responsable et Durable</h2>
+            <p>Greenroots ne se contente pas de planter des arbres. Notre mission est 
+                d'adopter une approche holistique de la reforestation, 
+                en intégrant des principes de durabilité, 
+                de respect des communautés locales et de biodiversité.</p>
+
+            <p>Nous nous engageons à :
+                <ul>
+                    <li>Planter des arbres adaptés aux écosystèmes locaux,
+                         favorisant la biodiversité et la résilience des forêts.</li>
+                    <li>Collaborer avec des partenaires locaux, des ONG et des communautés pour garantir que chaque arbre planté 
+                        bénéficie non seulement à l'environnement, mais aussi aux populations locales.</li>
+                    <li>Suivre et entretenir les forêts plantées, afin de s'assurer de leur 
+                        croissance et de leur contribution à la séquestration du carbone à long terme.</li>
+                </ul></p>
+            
+
+            <h2 className="p-2  h3-title text-greenRegular">Notre Vision pour l'Avenir</h2>
+            <p>Nous avons l'ambition de devenir un acteur majeur de la reforestation mondiale, avec des projets dans de nouvelles régions et une expansion de nos actions dans
+                 les zones les plus touchées par la déforestation et le dérèglement climatique.</p>
+            <p>En collaborant avec des partenaires internationaux et des organisations locales, nous continuerons à développer des solutions durables
+                 pour garantir que les forêts que nous plantons aujourd'hui prospéreront demain.</p>
+
+
+                <h2 className="p-2  h3-title text-greenRegular">Une Histoire de Passion et d'Engagement</h2>
+            <p>Depuis nos débuts modestes, Greenroots s’est transformé en une organisation internationale de reforestation, avec une mission claire : reconstruire les forêts du monde et protéger notre avenir commun. Nous croyons fermement que l’action collective est la clé pour relever les défis environnementaux globaux, 
+                et nous invitons chacun à rejoindre notre cause pour faire la différence.</p>
+        </div>
+
+    )
+}
+
+export default IntroducGr
