5629b4dae87ec9200a6f23a478dc22d1956524b0
Nhi-Mehl <nhi.mehl@gmail.com>
2024-09-05 09:20:14 +0200

    feat: :sparkles: Add some style DetailTreePage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,6 +8,15 @@ import HomePage from '../HomePage/HomePage';
 import DetailTreePage from '../DetailTreePage/DetaiTreePage';
 
 function App() {
+  // const getProjects = async () => {
+  //   try {
+  //     const response = await fetch('http://localhost:3000/projects');
+
+  //   } catch (error) {
+  //     console.log('catch/error pour projects', error);
+  //   }
+  // };
+
   return (
     <div className="app">
       <Header />
@@ -24,3 +33,21 @@ function App() {
 }
 
 export default App;
+
+// const [data, setData] = useState<Subject[] | null>(null);
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const response = await fetch(
+//         `${import.meta.env.VITE_API_URL}/subjects`
+//       );
+//       const data = await response.json();
+//       setData(data);
+//     } catch (error) {
+//       console.error("Error fetching matière:", error);
+//     }
+//   };
+
+//   fetchData();
+// }, []);
