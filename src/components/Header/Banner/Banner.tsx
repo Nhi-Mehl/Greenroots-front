644fe30ee5a27f4931dde13ce6f2258e9ec4ad26
Nhi-Mehl <nhi.mehl@gmail.com>
2024-09-13 17:01:59 +0200

    feat: :sparkles: Show banner component depend on route, Show title on picture banner depend on route

--- src/components/Header/Banner/Banner.tsx
+++ src/components/Header/Banner/Banner.tsx
@@ -19,29 +19,48 @@ function Banner() {
     navigate('/register');
   };
 
+  const getTitleForRoute = () => {
+    switch (location.pathname) {
+      case '/projects':
+        return (
+          <div className="absolute inset-0 flex flex-col gap-10 items-center justify-center text-white">
+            <h1 className="h1-title text-center mt-8">Notre Projets</h1>
+          </div>
+        );
+
+      case '/':
+        return (
+          <div className="absolute inset-0 flex flex-col gap-10 items-center justify-center text-white">
+            <h1 className="h1-title text-center mt-8">
+              Plantez aujourd’hui,
+              <span className="block"> protégez demain</span>
+            </h1>
+            <p className="text-2xl text-center mt-6">
+              Rejoignez notre communauté pour un avenir durable où chaque arbre
+              planté
+              <span className="block">
+                fait germer l'espoir d'une planète plus verte
+              </span>
+            </p>
+            <button className="btn mt-6" type="button" onClick={handleRegister}>
+              S’inscrire
+            </button>{' '}
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div>
+    <div className="relative">
       <img
         className="w-full lg:max-h-[800px] object-cover object-top"
         src={getImageForRoute()}
         alt="banner"
       />
-      <div className="absolute inset-0 flex flex-col gap-10 items-center justify-center text-white">
-        <h1 className="h1-title text-center mt-8">
-          Plantez aujourd’hui,
-          <span className="block"> protégez demain</span>
-        </h1>
-        <p className="text-2xl text-center mt-6">
-          Rejoignez notre communauté pour un avenir durable où chaque arbre
-          planté{' '}
-          <span className="block">
-            fait germer l'espoir d'une planète plus verte
-          </span>
-        </p>
-        <button className="btn mt-6" type="button" onClick={handleRegister}>
-          S’inscrire
-        </button>
-      </div>
+      {getTitleForRoute()}
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
+import { useLocation } from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import Banner from './Banner/Banner';
 
 function Header() {
+  const { pathname } = useLocation(); // Récupération de la route actuelle
+
+  // Routes sur lesquelles le composant Banner doit être affiché
+  const routesWithBanner = ['/', '/projects', '/project'];
+
+  // Vérifie si la route actuelle nécessite d'afficher le Banner
+  const showBanner = routesWithBanner.includes(pathname);
+
   return (
-    <header>
+    <header className="relative">
       <NavBar />
-      <Banner />
+      {/* Affiche le Banner seulement si la route est dans routesWithBanner */}
+      {showBanner && <Banner />}
     </header>
   );
 }
