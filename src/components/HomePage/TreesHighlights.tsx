fa5002cd561774ba4371855b075c86db68b65d9e
Nhi-Mehl <nhi.mehl@gmail.com>
2024-09-18 11:43:53 +0200

    feat: :sparkles: Add function redirect projectDetail to plant button

--- src/components/HomePage/TreesHighlights.tsx
+++ src/components/HomePage/TreesHighlights.tsx
@@ -1,9 +1,17 @@
 import axios from 'axios';
-import Swal from 'sweetalert2';
-import { useContext, useEffect, useState } from 'react';
+// import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import api from '../../api/index';
-import { CartContext } from '../Cart/CartContext/CartContext';
-import { useProject } from '../../context/ProjectContext';
+// import { CartContext } from '../Cart/CartContext/CartContext';
+// import { useProject } from '../../context/ProjectContext';
+
+const createSlug = (name: string) => {
+  return name
+    .toLowerCase()
+    .replace(/ /g, '-')
+    .replace(/[^\w-]+/g, '');
+}; // Renommé en IProject
 
 interface TreeHighlightsProps {
   id: number;
@@ -15,16 +23,17 @@ interface TreeHighlightsProps {
   price: string;
   picture: string;
   co2_compensation: string;
+  name: string;
+  project_id: number;
 }
 
 function TreesHighlights() {
-  const { addToCart } = useContext(CartContext);
+  // const { addToCart } = useContext(CartContext);
+
+  const navigate = useNavigate();
   const [treesHighlights, setTreeHighlighs] = useState<TreeHighlightsProps[]>(
     []
   );
-
-  const { project } = useProject();
-  console.log('Project', project);
 
   const getTreesHighlights = async () => {
     try {
@@ -47,19 +56,28 @@ function TreesHighlights() {
     getTreesHighlights();
   }, []);
 
-  const handleAddtoCart = (tree: TreeHighlightsProps) => {
-    if (!addToCart) {
-      console.error('addtoCart is not defined');
-      return;
-    }
-    addToCart(tree, project.name);
+  // const handleAddtoCart = (tree: TreeHighlightsProps) => {
+  //   if (!addToCart) {
+  //     console.error('addtoCart is not defined');
+  //     return;
+  //   }
+  //   addToCart(tree, project.name);
 
-    Swal.fire({
-      position: 'center',
-      icon: 'success',
-      title: 'Ajouter au panier',
-      showConfirmButton: false,
-      timer: 2000,
+  //   Swal.fire({
+  //     position: 'center',
+  //     icon: 'success',
+  //     title: 'Ajouter au panier',
+  //     showConfirmButton: false,
+  //     timer: 2000,
+  //   });
+  // };
+
+  const handleDetailTree = (tree: TreeHighlightsProps) => {
+    const projectName = tree.name;
+    // const treeName = tree.tree_name;
+    const slug = createSlug(projectName);
+    navigate(`/projects/${tree.project_id}/${slug}`, {
+      state: { tree, projectName },
     });
   };
 
@@ -82,11 +100,11 @@ function TreesHighlights() {
             <h2 className="h2-title text-center mt-10">{tree.tree_name}</h2>
             <p className="mt-10 px-10 text-justify">{tree.description}</p>
             <div className="flex justify-between items-center p-10">
-              <p className="text-xl md:text-2xl lg:text-3xl">{tree.price}€</p>
+              {/* <p className="text-xl md:text-2xl lg:text-3xl">{tree.price}€</p> */}
               <button
                 className="btn lg:text-lg"
                 type="button"
-                onClick={() => handleAddtoCart(tree, project?.name)}
+                onClick={() => handleDetailTree(tree, tree.name)}
               >
                 Planter
               </button>
