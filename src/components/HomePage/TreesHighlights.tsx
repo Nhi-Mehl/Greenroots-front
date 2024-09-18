a47ebdec36de348ac14e2eaade6ba56254f7fe90
Nhi-Mehl <nhi.mehl@gmail.com>
2024-09-18 10:01:31 +0200

    feat: :sparkles: Add function plant treesHighlight with bug

--- src/components/HomePage/TreesHighlights.tsx
+++ src/components/HomePage/TreesHighlights.tsx
@@ -1,16 +1,36 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
+import { useContext, useEffect, useState } from 'react';
 import api from '../../api/index';
-import { ISpecies } from '../../@types';
+import { CartContext } from '../Cart/CartContext/CartContext';
+import { useProject } from '../../context/ProjectContext';
+
+interface TreeHighlightsProps {
+  id: number;
+  tree_name: string;
+  basic_quantity: number;
+  current_quantity: number;
+  sold_quantity: number;
+  description: string;
+  price: string;
+  picture: string;
+  co2_compensation: string;
+}
 
 function TreesHighlights() {
-  const [treesHighlights, setTreeHighlighs] = useState<ISpecies[]>([]);
+  const { addToCart } = useContext(CartContext);
+  const [treesHighlights, setTreeHighlighs] = useState<TreeHighlightsProps[]>(
+    []
+  );
+
+  const { project } = useProject();
+  console.log('Project', project);
 
   const getTreesHighlights = async () => {
     try {
-      const response = await api.get('/projects/highlights');
+      const response = await api.get('/project_trees/highlights');
 
-      console.log(response);
+      console.log('HighLight trees', response.data);
 
       if (response.status === 200) {
         setTreeHighlighs(response.data);
@@ -27,6 +47,22 @@ function TreesHighlights() {
     getTreesHighlights();
   }, []);
 
+  const handleAddtoCart = (tree: TreeHighlightsProps) => {
+    if (!addToCart) {
+      console.error('addtoCart is not defined');
+      return;
+    }
+    addToCart(tree, project.name);
+
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Ajouter au panier',
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  };
+
   return (
     <>
       <h1 className="h1-title text-center mt-10 lg:text-5xl lg:mt-24">
@@ -41,13 +77,17 @@ function TreesHighlights() {
             <img
               className="w-full object-cover lg:w-lg lg:max-h-80"
               src={`/images/species/${tree.picture}.webp`}
-              alt={tree.name}
+              alt={tree.tree_name}
             />
-            <h2 className="h2-title text-center mt-10">{tree.name}</h2>
+            <h2 className="h2-title text-center mt-10">{tree.tree_name}</h2>
             <p className="mt-10 px-10 text-justify">{tree.description}</p>
             <div className="flex justify-between items-center p-10">
-              <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl">5€</p>
-              <button className="btn lg:text-lg" type="button">
+              <p className="text-xl md:text-2xl lg:text-3xl">{tree.price}€</p>
+              <button
+                className="btn lg:text-lg"
+                type="button"
+                onClick={() => handleAddtoCart(tree, project?.name)}
+              >
                 Planter
               </button>
             </div>
