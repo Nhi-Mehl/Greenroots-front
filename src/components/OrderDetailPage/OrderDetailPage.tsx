247286d79d18f5f3b36057607ff275f9cf077840
Nhi-Mehl <nhi.mehl@gmail.com>
2024-09-19 17:19:22 +0200

    feat: :sparkles: function OrderDetailPage with bug

--- src/components/OrderDetailPage/OrderDetailPage.tsx
+++ src/components/OrderDetailPage/OrderDetailPage.tsx
@@ -1,58 +1,86 @@
-import { useEffect, useState } from "react";
-import { IOrder, IOrderLine } from "../../@types";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
-import { useUser } from "../../context/UserContext";
-import api from "../../api";
-import { useProject } from "../../context/ProjectContext";
+import { useEffect, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { IOrder, IOrderLine } from '../../@types';
+import { useUser } from '../../context/UserContext';
+import api from '../../api';
+import { useProject } from '../../context/ProjectContext';
 
+interface IOrderWithDate extends IOrder {
+  createdAt: string;
+}
 
 function OrderDetailPage() {
+  const { orderId } = useParams();
+  const [orderLines, setOrderLines] = useState<IOrderLine[]>([]);
+  const [orderDetails, setOrderDetails] = useState<IOrderWithDate | null>(null);
+  const { project } = useProject();
+  console.log('project', project);
 
-  const { orderId } = useParams<{ orderId: number}>()
-  const [ orderLines, setOrderLines ] = useState<IOrderLine[]>([])
-  const [orderDetails, setOrderDetails] = useState<IOrder | null>(null);
-  const { project } = useProject()
-    const { user } = useUser()
-    if (!user) {
-      return <Navigate to= "/login" replace/> 
-    }
+  const { user } = useUser();
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
 
-    useEffect(() => {
-      console.log(user);
+  useEffect(() => {
+    console.log(user);
 
-      // Récupération des lignes de commande
-      const fetchOrderLines = async ()=> {
-        try {
-          const orderLinesResponse = await api.get(`/order_line/${orderId}`)
-          setOrderLines(orderLinesResponse.data)
+    // Récupération des lignes de commande
+    const fetchOrderLines = async () => {
+      try {
+        // Récupération des détails de la commande
+        const orderResponse = await api.get(`/orders/${user.id}`);
+        console.log(orderResponse);
+        setOrderDetails(orderResponse.data);
+
+        const orders = orderResponse.data;
+
+        orders.forEach(async (order: IOrder) => {
+          const orderLinesResponse = await api.get(`/order_line/${order.id}`);
+          setOrderLines(orderLinesResponse.data);
           console.log(orderLinesResponse);
-          
-
-          // Récupération des détails de la commande 
-          const orderResponse = await api.get(`/orders/${orderId}`); 
-          setOrderDetails(orderResponse.data);
-          console.log(orderResponse);
-          
-        } catch (error) {
-          console.error(
-            "Erreur lors de la récupération des détails de la commande"
-          );
-        }
+        });
+      } catch (error) {
+        console.error(
+          'Erreur lors de la récupération des détails de la commande'
+        );
       }
-      fetchOrderLines()
-    }, [orderId])
+    };
+    fetchOrderLines();
+  }, [user, orderId]);
 
+  console.log('orders', orderDetails);
   
+  const orderLineId = orderLines.forEach((orderLine) => orderLine.order_id;
+
+  const orderWithId = orderDetails.find(
+    (order: IOrderWithDate) =>
+      order.id === orderLineId)
+  );
+
   return (
     <div className="m-10">
-      <h1 className="text-center text-xl font-bold mb-4">
-        Commande numéro {orderDetails?.id}
-      </h1>
-      <p className="text-center mb-8">Date de la commande : {orderDetails?.date}</p>
-      
+      {/* {orderDetails?.map((order: IOrderWithDate) => (
+        <div key={order.id}>
+          <h1 className="text-center text-xl font-bold mb-4">
+            Commande numéro {order.id}
+          </h1>
+          <p className="text-center mb-8">
+            Date de la commande :{' '}
+            {new Date(order.createdAt).toLocaleDateString('fr-FR', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </p>
+        </div>
+      ))} */}
+
       <div className="space-y-8">
         {orderLines.map((orderLine) => (
-          <div key={orderLine.id} className="bg-gray-200 p-4 rounded-md shadow-md">
+          <div
+            key={orderLine.id}
+            className="bg-gray-200 p-4 rounded-md shadow-md"
+          >
             <p className="mb-2">
               <strong>Nom du projet :</strong> {project?.name}
             </p>
@@ -63,15 +91,20 @@ function OrderDetailPage() {
               <strong>Montant Unitaire :</strong> {orderLine.amount} €
             </p>
             <div className="flex justify-between">
-              <p><strong>Quantité :</strong> {orderLine.quantity}</p>
-              <p><strong>Montant Total :</strong> {orderLine.amount} €</p>
+              <p>orderLine.order_
+                <strong>Quantité :</strong> {orderLine.quantity}
+              </p>
+              <p>
+                <strong>Montant Total :</strong> {orderLine.amount} €
+              </p>
             </div>
           </div>
         ))}
       </div>
-      
+
       <p className="text-right font-bold mt-8">
-        Montant Total de la commande : { orderDetails?.amount || 'Montant indisponible'} €
+        Montant Total de la commande :
+        {orderDetails?.amount || 'Montant indisponible'} €
       </p>
     </div>
   );
--- src/components/OrdersPage/OrdersPage.tsx
+++ src/components/OrdersPage/OrdersPage.tsx
@@ -1,41 +1,39 @@
-import { useNavigate } from "react-router-dom";
-import { useUser } from "../../context/UserContext";
-import { useEffect, useState } from "react";
-import { IOrder } from "../../@types";
-import api from "../../api/index";
-
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useUser } from '../../context/UserContext';
+import { IOrder } from '../../@types';
+import api from '../../api/index';
 
 function OrdersPage() {
-const [orders, setOrders] = useState<IOrder[]>([])
-const navigate = useNavigate()
-const {user} = useUser()
+  const [orders, setOrders] = useState<IOrder[]>([]);
+  const navigate = useNavigate();
+  const { user } = useUser();
 
-if (!user) {
-  return <Navigate to="/login" replace />
-}
-console.log(user);
-
-useEffect(() => {
-  const fetchOrders = async () => {
-    try {
-      console.log("avant fetch");
-      
-      const response = await api.get(`/orders/${user.id}`); // Requête à l'API pour récupérer les commandes de l'utilisateur
-        console.log("Réponse API", response );
-      setOrders(response.data); // Mise à jour de l'état avec les données reçues
-    
-    console.log(response.data);
-    } catch (error) {
-      console.error();
-      ('Erreur lors de la récupération des commandes'); // Gère les erreurs
-    }
-  };
-
-  if (user?.id) {
-    fetchOrders();
+  if (!user) {
+    return <Navigate to="/login" replace />;
   }
-  
-}, [user.id]); // Exécute le useEffect quand l'utilisateur change
+  console.log(user);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        console.log('avant fetch');
+
+        const response = await api.get(`/orders/${user.id}`); // Requête à l'API pour récupérer les commandes de l'utilisateur
+        console.log('Réponse API', response);
+        setOrders(response.data); // Mise à jour de l'état avec les données reçues
+
+        console.log(response.data);
+      } catch (error) {
+        console.error();
+        ('Erreur lors de la récupération des commandes'); // Gère les erreurs
+      }
+    };
+
+    if (user?.id) {
+      fetchOrders();
+    }
+  }, [user.id]); // Exécute le useEffect quand l'utilisateur change
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -53,51 +51,53 @@ useEffect(() => {
     }
   }, [user?.id]); // Exécute lee useEffect quand l'utilisateur change
 
-
-return (
-  <main className="max-w-7xl mx-auto p-10">
-    <h1 className="text-center h2-title">Mes commandes</h1>
-    {orders.length === 0 ? (
-      <p>Aucune commande trouvée</p>
-    ) : (
-      <table className="table-auto w-full mt-10">
-        <thead>
-          <tr className="bg-beige text-left">
-            <th className="py-4 px-6">Commande</th>
-            <th className="py-4 px-6">Numéro</th>
-            <th className="py-4 px-6">Date</th>
-            <th className="py-4 px-6">Montant</th>
-            <th className="py-4 px-6">Détails</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order) => (
-            <tr
-              key={order.id}
-              className="border-b border-gray-300 bg-beige"
-            >
-              <td className="py-4 px-6">Commande</td>
-              <td className="py-4 px-6">{order.id}</td>
-              <td className="py-4 px-6">{new Date(order.createdAt).toLocaleDateString('fr-FR', {
-                  year: 'numeric',
-                  month: 'long',
-                  day: 'numeric',
-                })}</td>
-              <td className="py-4 px-6">{order.amount} €</td>
-              <td className="py-4 px-6">
-                <button className="btn text-white py-2 px-4 rounded" type="button" onClick={() => {
-            navigate(`/orders-details/:id`);
-          }}>
-                  Consulter
-                </button>
-              </td>
+  return (
+    <main className="max-w-7xl mx-auto p-10">
+      <h1 className="text-center h2-title">Mes commandes</h1>
+      {orders.length === 0 ? (
+        <p>Aucune commande trouvée</p>
+      ) : (
+        <table className="table-auto w-full mt-10">
+          <thead>
+            <tr className="bg-beige text-left">
+              <th className="py-4 px-6">Commande</th>
+              <th className="py-4 px-6">Numéro</th>
+              <th className="py-4 px-6">Date</th>
+              <th className="py-4 px-6">Montant</th>
+              <th className="py-4 px-6">Détails</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    )}
-  </main>
-);
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order.id} className="border-b border-gray-300 bg-beige">
+                <td className="py-4 px-6">Commande</td>
+                <td className="py-4 px-6">{order.id}</td>
+                <td className="py-4 px-6">
+                  {new Date(order.createdAt).toLocaleDateString('fr-FR', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  })}
+                </td>
+                <td className="py-4 px-6">{order.amount} €</td>
+                <td className="py-4 px-6">
+                  <button
+                    className="btn text-white py-2 px-4 rounded"
+                    type="button"
+                    onClick={() => {
+                      navigate(`/orders-details/:id`);
+                    }}
+                  >
+                    Consulter
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </main>
+  );
 }
 
 export default OrdersPage;
