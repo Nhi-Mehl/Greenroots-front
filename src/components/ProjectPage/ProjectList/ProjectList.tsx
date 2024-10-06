8a5a62e6f247aa499d783d51557ac5edbab89f2d
Nhi-Mehl <nhi.mehl@gmail.com>
2024-10-06 23:48:28 +0200

    style: :lipstick: Add progress bar + update style projectList

--- src/components/ProjectPage/ProjectList/ProjectList.tsx
+++ src/components/ProjectPage/ProjectList/ProjectList.tsx
@@ -36,49 +36,50 @@ function ProjectList() {
   }, []);
 
   return (
-    <div className="flex flex-row flex-wrap justify-center mt-10 mb-10">
+    <div className="p-8 flex gap-16 flex-wrap justify-center lg:p-16 lg:flex-row md:flex-col">
       {projects.map((project) => (
-        <div key={project.id} className=" m-9 flex h-full max-w-max shadow-lg">
-          <article className="flex flex-col w-full justify-between 4-52 bg-orange-50">
-            <img
-              className="h-60 object-cover"
-              src={`images/projets/${project.picture}.jpg`}
-              alt=""
-            />
-            <div className="p-6 flex flex-col gap-6">
-              <h2 className="h3-title text-center font-bold">
-                {project.name} - {project.country}
-              </h2>
+        <article
+          key={project.id}
+          className="lg:max-w-[520px] bg-beige shadow-lg"
+        >
+          <img
+            className="w-full h-60 object-cover"
+            src={`images/projets/${project.picture}.jpg`}
+            alt={project.name}
+          />
+          <div className="p-6 flex flex-col gap-6 justify-between ">
+            <h2 className="font-montserrat text-xl font-bold text-center lg:text-2xl">
+              {project.name} - {project.country}
+            </h2>
 
-              <label htmlFor="file">
-                <progress
-                  className="lg:w-2/3 lg:mb-0 pr-6 mb-4"
-                  id="file"
-                  max="100"
-                  value="70"
-                >
-                  70%
-                </progress>
-                96 % arbres financés
-              </label>
-              <p>10 arbres disponibles pour planter</p>
-
-              <p>
-                {truncateDescription(project.description, DESCRIPTION_LIMIT)}
-              </p>
-              <button
-                className="btn m-auto lg:text-base"
-                type="button"
-                onClick={() => {
-                  const slug = createSlug(project.name);
-                  navigate(`/projects/${project.id}/${slug}`);
-                }}
+            <label htmlFor="file">
+              <progress
+                className="md:w-2/3 md:mb-0 pr-6 mb-4"
+                id="file"
+                max="100"
+                value="70"
               >
-                Suivre ce projet
-              </button>
-            </div>
-          </article>
-        </div>
+                70%
+              </progress>
+              96 % arbres financés
+            </label>
+            <p className="font-bold">10 arbres disponibles pour planter</p>
+
+            <p className="text-justify">
+              {truncateDescription(project.description, DESCRIPTION_LIMIT)}
+            </p>
+            <button
+              className="btn m-auto lg:text-base"
+              type="button"
+              onClick={() => {
+                const slug = createSlug(project.name);
+                navigate(`/projects/${project.id}/${slug}`);
+              }}
+            >
+              Suivre ce projet
+            </button>
+          </div>
+        </article>
       ))}
     </div>
   );
