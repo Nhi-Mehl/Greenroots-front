328e923c2d7a40f768b4ba55ccac4a7a1d31cc22
Nhi-Mehl <nhi.mehl@gmail.com>
2024-11-22 23:42:27 +0100

    refactor: :recycle: Change fetch to axios in ProjectDetails.tsx

--- src/components/HomePage/OurProjects/Projects.tsx
+++ src/components/HomePage/OurProjects/Projects.tsx
@@ -7,26 +7,7 @@ import api from '../../../api/index';
 function Projects() {
   const [projects, SetProjects] = useState<IProject[]>([]);
 
-  // Récupéré les 3 projets les plus vendus avec fetch
-  // useEffect(() => {
-  //   const fetchProjects = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         'http://localhost:3000/api/projects/highlights'
-  //       );
-
-  //       const data: IProject[] = await response.json();
-  //       SetProjects(data);
-  //     } catch (error) {
-  //       console.error('erreur erreur');
-  //     }
-  //   };
-
-  //   fetchProjects();
-  // }, []);
-
   // Récupéré les 3 projets les plus vendus avec axios
-
   useEffect(() => {
     const fetchProjects = async () => {
       try {
