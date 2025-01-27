db483077b1b9fc8c4d1fa2b5dc4c868daf9e21f0
Nhi-Mehl <nhi.mehl@gmail.com>
2025-01-27 14:54:41 +0100

    refactor: :recycle: Mettre à jour le slice d'authentification pour utiliser des types TypeScript et simplifier les actions de connexion et de déconnexion

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,19 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { GetProfileResponse, LoginResponse } from '../../api/apiSlice';
 
 // Définir les types TypeScript pour l'état utilisateur
-interface UserState {
+type InitialUserState = GetProfileResponse & {
   token: string | null;
-  id: number | null;
-  name: string | null;
-  email: string | null;
-}
+};
 
 // Définir l'état initial
-const initialState: UserState = {
+const initialState: InitialUserState = {
+  id: 0,
+  first_name: '',
+  last_name: '',
+  role: '',
+  email: '',
+  address: '',
+  zip_code: '',
+  city: '',
+  country: '',
+  phone_number: '',
   token: null,
-  id: null,
-  name: null,
-  email: null,
 };
 
 // Créer le slice utilisateur
@@ -22,32 +27,20 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     // Action pour définir les informations utilisateur
-    setUser: (
-      state,
-      action: PayloadAction<{
-        token: string;
-        id: number;
-        name: string;
-        email: string;
-      }>
-    ) => {
+    login: (state, action: PayloadAction<LoginResponse>) => {
       state.token = action.payload.token;
-      state.id = action.payload.id;
-      state.name = action.payload.name;
-      state.email = action.payload.email;
+      localStorage.setItem('token', action.payload.token);
     },
     // Action pour déconnecter l'utilisateur
-    clearUser: (state) => {
+    logout: (state) => {
       state.token = null;
-      state.id = null;
-      state.name = null;
-      state.email = null;
+      localStorage.removeItem('token');
     },
   },
 });
 
 // Exporter les actions générées
-export const { setUser, clearUser } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 
 // Exporter le reducer pour l'intégrer dans le store Redux
 export default authSlice.reducer;
