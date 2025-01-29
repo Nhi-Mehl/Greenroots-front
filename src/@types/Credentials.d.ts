515428565f8c9846dd1d3fc8ab2bcce289eee7fb
Nhi-Mehl <nhi.mehl@gmail.com>
2025-01-29 23:47:52 +0100

    feat: :sparkles: Ajouter des types pour les requêtes et réponses d'inscription et de connexion

--- src/@types/Credentials.d.ts
+++ src/@types/Credentials.d.ts
@@ -1,12 +1,19 @@
 import { IUser } from './IUser';
 
+// Types données utilisateurs au mont de l'inscription
 export type SignUpRequest = Omit<IUser, 'id' | 'role'>;
 
+// Types données de la réponse de l'inscription
 export type SignUpResponse = { message: string };
+
+// Types données de la requête de connexion
 export type LoginRequest = {
   email: string;
   password: string;
 };
-export type LoginResponse = { token: string };
 
+// Types données de la réponse de la connexion
+export type LoginResponse = { accessToken: string };
+
+// Types données de la réponse de la récupération du profil
 export type GetProfileResponse = Omit<IUser, 'password' | 'confirmation'>;
