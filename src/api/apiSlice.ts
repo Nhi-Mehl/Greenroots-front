9e92d7fd56244a83f935dc29ac36d215045c522d
Nhi-Mehl <nhi.mehl@gmail.com>
2025-01-29 23:48:12 +0100

    feat: :sparkles: Refactor l'API Slice pour ajouter des endpoints d'authentification

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -1,20 +1,6 @@
 import { createApi, BaseQueryFn } from '@reduxjs/toolkit/query/react';
 import { AxiosError, AxiosRequestConfig } from 'axios';
 import api from './api';
-import {
-  GetProfileResponse,
-  LoginRequest,
-  LoginResponse,
-  SignUpRequest,
-  SignUpResponse,
-} from '../@types/Credentials';
-
-// type BaseQueryParams = {
-//   url: string;
-//   method?: AxiosRequestConfig['method'];
-//   data?: AxiosRequestConfig['data'];
-//   params?: AxiosRequestConfig['params'];
-// };
 
 // Définir un BaseQuery personnalisé avec Axios
 const axiosBaseQuery: BaseQueryFn<
@@ -50,40 +36,10 @@ const axiosBaseQuery: BaseQueryFn<
 };
 
 // Création de l'API Slice avec des endpoints
-export const apiSlice = createApi({
+const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: axiosBaseQuery,
-  endpoints: (builder) => ({
-    // Endpoint pour la connexion
-    login: builder.mutation<LoginResponse, LoginRequest>({
-      query: (credentials: LoginRequest) => ({
-        url: '/auth/login',
-        method: 'post',
-        data: { ...credentials },
-      }),
-    }),
-    // Endpoint pour l'inscription
-    register: builder.mutation<SignUpResponse, SignUpRequest>({
-      query: (credentials: SignUpRequest) => ({
-        url: '/auth/register',
-        method: 'post',
-        data: { ...credentials },
-      }),
-    }),
-    // Endpoint pour récupérer le profil utilisateur
-    getProfile: builder.query<GetProfileResponse, void>({
-      query: () => ({
-        url: '/users/profile',
-        method: 'get',
-      }),
-    }),
-    // protected: builder.mutation<{ message: string }, void>({
-    //   query: () => 'protected',
-    // }),
-  }),
+  endpoints: () => ({}),
 });
 
-// RTK Query automatically generates hooks for each endpoint
-// as use{EndpointName}Query or use{EndpointName}Mutation
-export const { useLoginMutation, useRegisterMutation, useGetProfileQuery } =
-  apiSlice;
+export default apiSlice;
---
+++ src/api/authApiSlice.ts
@@ -0,0 +1,45 @@
+import apiSlice from './apiSlice';
+import {
+  GetProfileResponse,
+  LoginRequest,
+  LoginResponse,
+  SignUpRequest,
+  SignUpResponse,
+} from '../@types/Credentials';
+
+// Étendre apiSlice pour ajouter des endpoints spécifiques à l'authentification
+const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // Endpoint pour la connexion
+    login: builder.mutation<LoginResponse, LoginRequest>({
+      query: (credentials: LoginRequest) => ({
+        url: '/auth/login',
+        method: 'post',
+        data: { ...credentials },
+      }),
+    }),
+    // Endpoint pour l'inscription
+    register: builder.mutation<SignUpResponse, SignUpRequest>({
+      query: (credentials: SignUpRequest) => ({
+        url: '/auth/register',
+        method: 'post',
+        data: { ...credentials },
+      }),
+    }),
+    // Endpoint pour récupérer le profil utilisateur
+    getProfile: builder.query<GetProfileResponse, void>({
+      query: () => ({
+        url: '/users/profile',
+        method: 'get',
+      }),
+    }),
+    // protected: builder.mutation<{ message: string }, void>({
+    //   query: () => 'protected',
+    // }),
+  }),
+});
+
+// RTK Query automatically generates hooks for each endpoint
+// as use{EndpointName}Query or use{EndpointName}Mutation
+export const { useLoginMutation, useRegisterMutation, useGetProfileQuery } =
+  authApiSlice;
