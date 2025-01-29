acefc9ed1cef5d68b29d22c769c6cce353e7bf1d
Nhi-Mehl <nhi.mehl@gmail.com>
2025-01-29 23:49:20 +0100

    feat: :sparkles: Refactor MyAccountPage pour utiliser le sélecteur d'utilisateur depuis authSlice et simplifier la logique de déconnexion

--- src/components/pages/MyAccount/MyAccount.tsx
+++ src/components/pages/MyAccount/MyAccount.tsx
@@ -1,26 +1,22 @@
-import { useNavigate, Navigate } from 'react-router-dom';
-import { useUser } from '../../../context/UserContext';
-import api from '../../../api';
+import { useNavigate } from 'react-router-dom';
+import { selectCurrentUser } from '../../../features/auth/authSlice';
+import { useAppSelector } from '../../../store/hooks';
 
 function MyAccountPage() {
   const navigate = useNavigate();
+  const user = useAppSelector(selectCurrentUser);
 
-  const { user, setUser } = useUser();
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-
-  const handleLogout = async () => {
-    await api.post('/auth/logout'); // On envoie une requête POST à l'API pour déconnecter l'utilisateur et invalider le token
-    localStorage.removeItem('token'); // On supprime le token du stockage local
-    setUser(null); // On réinitialise l'utilisateur dans le contexte
-    navigate('/login'); // On redirige l'utilisateur vers la page de connexion
-  };
+  // const handleLogout = async () => {
+  //   await api.post('/auth/logout'); // On envoie une requête POST à l'API pour déconnecter l'utilisateur et invalider le token
+  //   localStorage.removeItem('token'); // On supprime le token du stockage local
+  //   setUser(null); // On réinitialise l'utilisateur dans le contexte
+  //   navigate('/login'); // On redirige l'utilisateur vers la page de connexion
+  // };
 
   return (
     <main className="flex flex-col justify-center min-h-screen">
-      <h2 className="h2-title text-center">Mon Compte</h2>
-      <h3 className="text-center text-2xl">Bonjour {user.first_name}</h3>
+      <h2 className="h2-title text-center">Mon Compte {user?.first_name}</h2>
+      <h3 className="text-center text-2xl">Bonjour</h3>
       <div className="flex flex-col items-center gap-4 my-10 md:my-20 md:justify-center md:gap-10 md:flex-row">
         <button
           className="btn w-full"
@@ -40,7 +36,7 @@ function MyAccountPage() {
         >
           Mes commandes
         </button>
-        <button className="btn w-full" type="button" onClick={handleLogout}>
+        <button className="btn w-full" type="button">
           Déconnexion
         </button>
       </div>
