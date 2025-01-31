0e3a69330c0208b8d42c405a023efc005792eeb1
Nhi-Mehl <nhi.mehl@gmail.com>
2025-01-31 10:21:16 +0100

    feat: :sparkles: Update type definitions for login and profile management; streamline request and response structures

--- src/@types/Credentials.d.ts
+++ src/@types/Credentials.d.ts
@@ -7,13 +7,7 @@ export type SignUpRequest = Omit<IUser, 'id' | 'role'>;
 export type SignUpResponse = { message: string };
 
 // Types données de la requête de connexion
-export type LoginRequest = {
-  email: string;
-  password: string;
-};
+export type LoginRequest = Pick<IUser, 'email' | 'password'>;
 
 // Types données de la réponse de la connexion
 export type LoginResponse = { accessToken: string };
-
-// Types données de la réponse de la récupération du profil
-export type GetProfileResponse = Omit<IUser, 'password' | 'confirmation'>;
--- src/@types/IUser.d.ts
+++ src/@types/IUser.d.ts
@@ -12,3 +12,20 @@ export interface IUser {
   password: string;
   confirmation: string;
 }
+
+// Types données de la réponse de la récupération du profil
+export type GetProfileResponse = Omit<IUser, 'password' | 'confirmation'>;
+
+// Types données de la requête de mise à jour du profil
+export type UpdateProfileRequest = Required<Pick<IMember, 'id'>> &
+  Partial<Omit<IUser, 'password' | 'confirmation' | 'role'>>;
+
+// Types données de la réponse de mise à jour du profil
+export type UpdateProfileResponse = {
+  sucess: boolean;
+  message: string;
+  data: Omit<IUser, 'confirmation' | 'password'> & {
+    createdAt: string;
+    updatedAt: string;
+  };
+};
