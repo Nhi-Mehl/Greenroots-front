f7543c1ce11fa0815020007c99ded41ff805f355
Nhi-Mehl <nhi.mehl@gmail.com>
2025-02-01 00:03:32 +0100

    refactor: :sparkles: Update ProtectedRoute to use user profile query for authentication and loading state

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,15 +1,19 @@
 // This component will be used to protect routes that require authentication.
 // If the user is not authenticated, they will be redirected to the login page.
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAppSelector } from '../store/hooks';
-import { selectCurrentUser } from '../store/features/auth/authSlice';
+import { useGetProfileQuery } from '../store/features/user/userApiSlice';
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const location = useLocation();
-  const user = useAppSelector(selectCurrentUser);
+  const { data: user, isLoading, isError } = useGetProfileQuery();
+
+  // wait for data before deciding
+  if (isLoading) {
+    return <p>Chargement...</p>;
+  }
 
   // Redirect to /login if not authenticated or API error
-  if (!user) {
+  if (isError || !user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
