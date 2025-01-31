c3f6201d23a9a4686957dc223b529ae82598c8bb
Nhi-Mehl <nhi.mehl@gmail.com>
2025-01-31 15:16:18 +0100

    refactor: :sparkles: Update Input component styling for improved layout and user experience

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -14,7 +14,12 @@ const Input = ({ htmlFor, label, value, onChange, ...props }: InputProps) => {
   return (
     <label htmlFor={htmlFor}>
       {label}
-      <input {...props} value={value} onChange={onChange} className="input" />
+      <input
+        {...props}
+        value={value}
+        onChange={onChange}
+        className="w-full border border-gray-300 rounded-md mb-2 px-4 py-2 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-greenRegular md:mb-4"
+      />
     </label>
   );
 };
