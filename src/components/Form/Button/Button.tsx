f903a9b3635a7f8f962a8bcaebb049e887e1ae6d
Nhi-Mehl <nhi.mehl@gmail.com>
2025-02-06 22:03:58 +0100

    refactor: :sparkles: Update Button component to include 'form-danger' variant and adjust styling for improved visual consistency

--- src/components/Form/Button/Button.tsx
+++ src/components/Form/Button/Button.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   type?: 'button' | 'submit' | 'reset'; // Types standards des boutons
-  variant?: 'form' | 'danger' | 'default';
+  variant?: 'form' | 'form-danger' | 'default';
   className?: string;
   isLoading?: boolean;
 }
@@ -22,11 +22,12 @@ const Button = ({
     <button
       type={type}
       className={clsx(
-        'font-montserrat bg-greenRegular text-white font-semibold text-xs md:text-sm lg:text-base py-2 px-3 md:py-3 md:px-4 lg:py-4 lg:px-5',
+        'font-montserrat text-white font-semibold text-xs md:text-sm lg:text-base py-2 px-3 md:py-3 md:px-4 lg:py-4 lg:px-5',
         {
-          'w-full rounded-full': variant === 'form',
-          'w-full rounded-full bg-black': variant === 'danger',
-          'w-fit rounded-lg lg:rounded-xl': variant === 'default',
+          'bg-greenRegular w-full rounded-full': variant === 'form',
+          'bg-black w-full rounded-full': variant === 'form-danger',
+          'bg-greenRegular w-fit rounded-lg lg:rounded-xl':
+            variant === 'default',
         },
         className
       )}
