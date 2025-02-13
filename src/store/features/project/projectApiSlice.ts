791880bb9093d0a1801cf83dd277deeffeb5e99b
Nhi-Mehl <nhi.mehl@gmail.com>
2025-02-13 17:10:36 +0100

    feat: :sparkles: Refactor getProjectsByIds to use queryFn for improved project data fetching

--- src/store/features/project/projectApiSlice.ts
+++ src/store/features/project/projectApiSlice.ts
@@ -14,15 +14,39 @@ const projectApiSlice = apiSlice
       //   }),
       //   providesTags: ['Project'],
       // }),
+
+      // getProjectsByIds: builder.query<IProject[], number[]>({
+      //   query: (projectIds) => ({
+      //     url: `/projects`,
+      //     method: 'get',
+      //     params: { ids: projectIds.join(',') }, // Ex: ?ids=1,2,3
+      //   }),
+      //   providesTags: ['Project'],
+      // }),
+
       getProjectsByIds: builder.query<IProject[], number[]>({
-        query: (projectIds) => ({
-          url: `/projects`,
-          method: 'get',
-          params: { ids: projectIds.join(',') }, // Ex: ?ids=1,2,3
-        }),
+        queryFn: async (projectIds, _queryApi, _extraOptions, baseQuery) => {
+          try {
+            const responses = await Promise.all(
+              projectIds.map((projectId) =>
+                baseQuery({ url: `/projects/${projectId}` })
+              )
+            );
+
+            const projects = responses.map((response) => {
+              if ('data' in response) {
+                return response.data as IProject;
+              }
+              throw new Error('Failed to fetch project data');
+            });
+
+            return { data: projects };
+          } catch (error) {
+            return { error: { status: 500, data: (error as Error).message } };
+          }
+        },
         providesTags: ['Project'],
       }),
-
       // Endpoint pour récupérer 3 projets random de la page accueil
       getThreeRandomProjects: builder.query<IProject[], void>({
         query: () => ({
