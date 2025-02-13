90d9f783aafdd58ea64e72d61a517ebda54579c9
Nhi-Mehl <nhi.mehl@gmail.com>
2025-02-13 15:27:50 +0100

    feat: :sparkles: Add project API slice with endpoints for fetching projects by IDs and random projects

---
+++ src/store/features/project/projectApiSlice.ts
@@ -0,0 +1,37 @@
+import { IProject } from '../../../@types';
+import apiSlice from '../../api/apiSlice';
+
+const projectApiSlice = apiSlice
+  .enhanceEndpoints({ addTagTypes: ['Project'] })
+  .injectEndpoints({
+    // Endpoint pour récupérer le profil utilisateur
+    endpoints: (builder) => ({
+      // Endpoint pour récupérer les commandes d'un utilisateur via son id
+      // getProjectById: builder.query<IProject, number>({
+      //   query: (projectId) => ({
+      //     url: `/orders/${projectId}`,
+      //     method: 'get',
+      //   }),
+      //   providesTags: ['Project'],
+      // }),
+      getProjectsByIds: builder.query<IProject[], number[]>({
+        query: (projectIds) => ({
+          url: `/orders`,
+          method: 'get',
+          params: { ids: projectIds.join(',') }, // Ex: ?ids=1,2,3
+        }),
+        providesTags: ['Project'],
+      }),
+
+      // Endpoint pour récupérer 3 projets random de la page accueil
+      getThreeRandomProjects: builder.query<IProject[], void>({
+        query: () => ({
+          url: '/projects/highlights',
+          method: 'get',
+        }),
+      }),
+    }),
+  });
+
+export const { useGetProjectsByIdsQuery, useGetThreeRandomProjectsQuery } =
+  projectApiSlice;
