fd7ff0ee1272cbe6e4368dbc417d7a9d5a914f6d
Nhi-Mehl <nhi.mehl@gmail.com>
2025-02-27 12:17:21 +0100

    feat: :sparkles: Refactor TreeDetailsPage to use handleAddToCart utility and improve state management

--- src/components/pages/TreeDetails/TreeDetails.tsx
+++ src/components/pages/TreeDetails/TreeDetails.tsx
@@ -1,103 +1,95 @@
-import { useContext } from 'react';
 import { useLocation } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { IProjectTree } from '../../../@types/ProjectTree';
-import { CartContext } from '../../../context/CartContext/CartContext';
+
+import { useAppDispatch } from '../../../store/hooks';
+import handleAddToCart from '../../../utils/addToCart';
+import { IProjectTreeSpecies } from '../../../@types/ProjectTree';
+import Button from '../../Form/Button/Button';
+
+type StateProps = {
+  tree: IProjectTreeSpecies;
+  projectName: string;
+};
 
 function TreeDetailsPage() {
-  const location = useLocation();
-  const { addToCart } = useContext(CartContext);
-  console.log('Location state:', location.state);
-  const tree = location.state?.tree;
-  const projectName = location.state?.projectName;
-  console.log('Tree:', tree);
-  console.log('ProjectName:', projectName);
+  const dispatch = useAppDispatch();
+  const { state }: { state: StateProps } = useLocation();
+
+  if (!state || !state.tree) {
+    // Gérer le cas où les données sont manquantes
+    return (
+      <p role="alert">Erreur : Les détails de l&apos;arbre sont manquants.</p>
+    );
+  }
+
+  const { tree, projectName } = state;
 
   const baseQuantity = tree.basic_quantity;
   const currentQuantity = tree.current_quantity;
   const treePlanted = baseQuantity - currentQuantity;
 
-  const handleAddToCart = (treeProject: IProjectTree) => {
-    if (!projectName) {
-      console.error('Project name is not defined');
-      return;
-    }
-    console.log('Adding to cart:', { treeProject, projectName });
-
-    addToCart(treeProject, projectName);
-
-    Swal.fire({
-      position: 'center',
-      icon: 'success',
-      title: 'Ajouter au panier',
-      showConfirmButton: false,
-      timer: 2000,
-    });
-  };
-
   return (
-    <main className="flex flex-col gap-10">
-      <section className="w-full lg:mt-12 relative inline-block">
-        <img
-          className="w-full lg:w-9/12 h-64 lg:h-128 object-cover mx-auto"
-          src={`/images/species/${tree.species.picture}.webp`}
-          alt={tree.species.name}
-        />
-        <h2 className="absolute inset-0 flex items-center justify-center h1-title text-white">
-          {tree?.species.name}
-        </h2>
+    <main className="min-h-screen grid gap-8 xl:grid-flow-col xl:grid-rows-2 xl:gap-10 xl:p-10">
+      {/* Section de présentation de l’arbre */}
+      <section className="xl:row-span-2">
+        <figure>
+          <img
+            className="w-full h-64 sm:h-128 md:h-200 object-cover rounded-lg shadow-lg"
+            src={`/images/species/${tree.species.picture}.webp`}
+            alt={`Illustration de l'arbre ${tree.species.name}`}
+            loading="lazy"
+          />
+        </figure>
       </section>
-      <section className="text-lg lg:text-2xl w-10/12 xl:w-3/4 2xl:w-2/3 mx-auto p-4 lg:p-10 sectionText bg-white shadow-lg">
-        <p className="mb-4 font-semibold">
-          Nom de l’arbre :{' '}
-          <span className="font-normal text-greenRegular">
-            {tree?.species.name}
-          </span>
-        </p>
-        <p className="mb-4 font-semibold">
-          Nom scientifique :{' '}
-          <span className="text-greenRegular font-normal">
-            {tree?.species.scientific_name}
-          </span>
-        </p>
-        <p className="mb-4">{tree.species.description}</p>
-        <p className="mb-4 font-semibold">
-          Co2 :{' '}
-          <span className="text-greenRegular font-normal">
-            {' '}
-            Absobre {tree.species.co2_compensation} tonnes de co2 par an{' '}
-          </span>
-        </p>
-        <p className="mb-4 font-semibold">
-          Prix :{' '}
-          <span className="text-greenRegular font-normal">
-            {tree.species.price} €
-          </span>
-        </p>
-      </section>
-      <section className="flex flex-col gap-8 sectionText">
-        <div className="flex justify-center gap-4 sm:gap-36 md:gap-52">
-          <div className="text-center">
-            <p className="text-sm md:text-xl lg:text-2xl font-bold text-greenRegular">
-              Quantité disponible
-            </p>
-            <p className="text-lg md:text-2xl">{tree.current_quantity}</p>
-          </div>
 
-          <div className="text-center">
-            <p className="text-sm md:text-xl lg:text-2xl font-bold text-greenRegular">
-              Quantité planté
-            </p>
-            <p className="text-lg md:text-2xl">{treePlanted}</p>
+      {/* Section des informations de l’arbre */}
+      <article
+        aria-labelledby="Informations de l’arbre"
+        className="bg-white shadow-lg rounded-lg p-6 md:p-8 lg:p-10"
+      >
+        <header>
+          <h1 className="h1-title text-greenRegular">{tree.species.name}</h1>
+          <h2 className="h2-title italic text-gray-500">
+            {tree.species.scientific_name}
+          </h2>
+        </header>
+
+        <p className="text-lg sm:text-xl my-4">{tree.species.description}</p>
+
+        {/* Détails sous forme de liste de définition */}
+        <dl className="text-xl sm:text-2xl">
+          <div>
+            <dt className="font-semibold">CO₂ absorbé</dt>
+            <dd className="text-greenRegular">
+              {tree.species.co2_compensation} tonnes / an
+            </dd>
           </div>
+          <div>
+            <dt className="font-semibold">Prix</dt>
+            <dd className="text-greenRegular">{tree.species.price} €</dd>
+          </div>
+        </dl>
+      </article>
+
+      {/* Section Quantités disponibles et plantées */}
+      <section className="flex flex-col gap-4 items-center pb-10 lg:flex-row lg:justify-around">
+        <div className="text-center lg:order-1">
+          <h3 className="h3-title text-greenRegular">Quantité disponible</h3>
+          <p className="text-xl sm:text-2xl">{currentQuantity}</p>
         </div>
-        <button
-          className="btn block mx-auto mb-8"
+        <div className="text-center lg:order-3">
+          <h3 className="h3-title text-greenRegular">Quantité plantée</h3>
+          <p className="text-xl sm:text-2xl">{treePlanted}</p>
+        </div>
+
+        {/* Bouton d'ajout au panier */}
+        <Button
+          variant="default"
+          className="lg:order-2"
           type="button"
-          onClick={() => handleAddToCart(tree)}
+          onClick={() => handleAddToCart(tree, projectName, dispatch)}
         >
           Ajouter au panier
-        </button>
+        </Button>
       </section>
     </main>
   );
