56d48f0fad07bf08c3e1d955e1156c49ebc7769c
Nhi-Mehl <nhi.mehl@gmail.com>
2025-03-18 16:12:25 +0100

    feat: :sparkles: Configure Vite server for Docker compatibility with polling and external access

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permet l'accès depuis l'extérieur (contener)
+    port: 5173, // Assurez-vous d'utiliser le bon port (le même que dans Dockerfile)
+    watch: {
+      usePolling: true, // Utiliser le polling pour mieux gérer les changements dans Docker
+    },
+  },
 });
